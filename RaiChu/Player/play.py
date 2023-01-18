25a574e86b4555ce72e551d7ebbb7e0e4ccb3cd2
AADITYA PRASAD <122255434+onenonlybotz@users.noreply.github.com>
2023-01-18 13:27:07 +0530

    Update play.py

--- RaiChu/Player/play.py
+++ RaiChu/Player/play.py
@@ -171,7 +171,7 @@ async def play(c: Client, m: Message):
                       reply_markup=InlineKeyboardMarkup(
                     [
                         [
-                            InlineKeyboardButton("• Support", url=f"https://t.me/BotDuniyaXd"),
+                            InlineKeyboardButton("• Support", url=f"https://t.me/TH3ONLYSUPPORT"),
                             InlineKeyboardButton("• Close", callback_data="cls")
                         ]
                     ]
