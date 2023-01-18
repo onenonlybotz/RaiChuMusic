ff98183cdc672b942722a2ce40f584851e7240ee
AADITYA PRASAD <122255434+onenonlybotz@users.noreply.github.com>
2023-01-18 13:25:06 +0530

    Update inline.py

--- RaiChu/inline.py
+++ RaiChu/inline.py
@@ -64,7 +64,7 @@ def menu_markup(user_id):
       InlineKeyboardButton(text="▢", callback_data=f'cbstop | {user_id}')
     ],
      [InlineKeyboardButton(text="🔇", callback_data=f'cbmute | {user_id}'),
-      InlineKeyboardButton(text="ᴜᴩᴅᴀᴛᴇs", url=f"https://t.me/BotDuniyaXd"),
+      InlineKeyboardButton(text="ᴜᴩᴅᴀᴛᴇs", url=f"https://t.me/TH3ONLYCHANNEL"),
       InlineKeyboardButton(text="🔊", callback_data=f'cbunmute | {user_id}')],
   ]
   return buttons
